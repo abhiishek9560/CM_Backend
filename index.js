9e3cda0a316aa7da1eaea56d5372bfd58170f8e1
abhiishek9560 <sumityadavanshi9560@gmail.com>
2025-09-14 15:37:48 +0530

    Updated cors to allow vercel

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const port = 3000
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
-
+const allowedOrigins = [
+    "http://localhost:5173",
+    "cm-frontend-virid.vercel.app"
+]
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true                
+    origin: function(origin, callback){
+        if(!origin || allowedOrigins.includes(origin)){
+            callback(null, true);
+        }else{
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials:true
 }));
 const connectDB = require("./db");
 
